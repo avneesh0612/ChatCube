55e3cf221a56058c710ddb9cea71090ff08821cf
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-22 12:10:42 +0530

    message fix

--- components/Message.js
+++ components/Message.js
@@ -5,17 +5,17 @@ function Message({ message, user }) {
   const TypeOfMessage = user === userLoggedIn ? "Sender" : "Reciever";
 
   return (
-    <div>
+    <div className="min-w-[80px]">
       <p
         style={{ width: "fit-content" }}
-        className={`p-4 rounded-lg m-3 min-w-[60px] pb-7 relative text-right break-all text-white ${
+        className={`p-4 rounded-lg m-3 min-w-[80px] pb-7 relative text-right break-all text-white ${
           TypeOfMessage === "Sender"
             ? "ml-auto bg-indigo-900 "
             : "bg-blue-900 text-left"
         }`}
       >
         {message.message}
-        <p className="text-gray-400 p-2 text-xs absolute bottom-0 text-right right-0 mt-3">
+        <p className="text-gray-400 min-w-[80px] p-2 text-xs absolute bottom-0 text-right right-0 mt-3">
           {message.timestamp ? moment(message.timestamp).format("LT") : "..."}
         </p>
       </p>
