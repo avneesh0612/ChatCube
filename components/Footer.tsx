dd69e5cb4f167cbc705c666501d8f26c5b0cade2
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-07-28 10:57:20 +0530

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import Fade from "react-reveal/Fade";
+
+function Header() {
+  return (
+    <Fade bottom>
+      <footer className="flex items-center justify-between px-5 m-4 bg-white dark:bg-indigo-700  mb-5 dark:text-white rounded-xl">
+        <p className="text-center w-full py-3 text-lg font-medium">
+          Made with 💜 by{" "}
+          <a href="https://avneesh-links.vercel.app/">Avneesh Agarwal</a>
+        </p>
+      </footer>
+    </Fade>
+  );
+}
+
+export default Header;
