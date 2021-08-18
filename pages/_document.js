971822c07fa342ed3f9597707e4bd61d96d6a409
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2021-08-18 16:26:24 +0530

    added improvements

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/Icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <script
+          data-ad-client="ca-pub-8226340494391016"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        ></script>
         </Head>
         <body>
           <Main />
