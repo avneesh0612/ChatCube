cc3f043309192b1c2e073883e4ddf73bc57f561a
Your Name <avneeshagarwal0612@gmail.com>
2021-09-15 14:58:56 +0530

    bug fix

--- utils/getRecipientEmail.ts
+++ utils/getRecipientEmail.ts
@@ -1,4 +1,4 @@
-const getRecipientEmail = (users: [string], userLoggedIn: string) =>
-  users?.filter((userToFilter: string) => userToFilter !== userLoggedIn)[0];
+const getRecipientEmail = (users: [string | undefined], userLoggedIn: string | undefined) =>
+  users?.filter((userToFilter: string | undefined) => userToFilter !== userLoggedIn)[0];
 
 export default getRecipientEmail;
