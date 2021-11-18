8b900ed602cbe7bd3e0f7e840e72eab90a39c1be
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-18 22:39:40 +0530

    ci(standard-version): added automated changelogs with standard-version

---
+++ .versionrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  types: [
+    { type: "feat", "section": "Features" },
+    { type: "fix", "section": "Bug Fixes" },
+    { type: "chore", "hidden": true },
+    { type: "docs", "hidden": true },
+    { type: "style", "hidden": true },
+    { type: "refactor", "hidden": true },
+    { type: "perf", "hidden": true },
+    { type: "test", "hidden": true }
+  ],
+  "commitUrlFormat": "https://github.com/AnishDe12020/devkit/commits/{{hash}}",
+  "compareUrlFormat": "https://github.com/AnishDe12020/devkit/compare/{{previousTag}}...{{currentTag}}"
+}
